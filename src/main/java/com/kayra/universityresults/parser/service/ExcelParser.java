b98fd92aac6521d361b5333491ee9cde8a238762
Gürhan Küçük <gurhan.kucuk@yandex.com>
2017-04-09 20:16:04 +0300

    added excel parser interface.

---
+++ src/main/java/com/kayra/universityresults/parser/service/ExcelParser.java
@@ -0,0 +1,24 @@
+package com.kayra.universityresults.parser.service;
+
+import org.apache.poi.ss.usermodel.Row;
+
+import com.kayra.universityresults.parser.constant.RowType;
+import com.kayra.universityresults.parser.model.City;
+import com.kayra.universityresults.parser.model.Department;
+import com.kayra.universityresults.parser.model.Faculty;
+import com.kayra.universityresults.parser.model.University;
+
+public interface ExcelParser {
+
+	public RowType getRowType(Row row);
+
+	public City getCity(Row row);
+
+	public University getUniversity(Row row);
+	
+	public Faculty getFaculty(Row row);
+	
+	public Department createDepartment(Row row);
+
+	
+}
