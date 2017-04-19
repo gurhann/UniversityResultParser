e82b9817c790771dc3b32b1269f8e0b24545b77a
Gürhan Küçük <gurhan.kucuk@yandex.com>
2017-04-19 19:57:31 +0300

    added department column and app constants.

---
+++ src/main/java/com/kayra/universityresults/parser/constant/AppConstant.java
@@ -0,0 +1,12 @@
+package com.kayra.universityresults.parser.constant;
+
+public class AppConstant {
+
+	private AppConstant() {
+
+	}
+
+	public static int NULL_NUMBER_COLUMN = -1;
+	public static int ZERO_NUMBER_COLUMN = 0;
+	public static String ENGLISH_DEPARTMENT = "(İngilizce)";
+}
---
+++ src/main/java/com/kayra/universityresults/parser/constant/DepartmentColumn.java
@@ -0,0 +1,6 @@
+package com.kayra.universityresults.parser.constant;
+
+public enum DepartmentColumn {
+
+	ID, NAME, QUOTA, SETTLED, SCORE_TYPE, MIN_POINT, SUCCESS_SEQUENCE, MAX_POINT, TOP_STUDENT_OF_SCHOOL_MIN_POINT, TOP_STUDENT_OF_SCHOOL_MAX_POINT;
+}
