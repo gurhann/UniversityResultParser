35a35064cfd4e60a3f084aab0f3f5e83071da3fd
Gürhan Küçük <gurhan.kucuk@yandex.com>
2017-06-15 04:15:31 +0300

    added string util class for null object to string method.

---
+++ src/main/java/com/kayra/universityresults/parser/util/StringUtil.java
@@ -0,0 +1,11 @@
+package com.kayra.universityresults.parser.util;
+
+public class StringUtil {
+
+	public static String toString(Object obj) {
+		if (obj == null) {
+			return null;
+		}
+		return obj.toString();
+	}
+}
